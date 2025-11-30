a4f65effe0ed664a274c69521cf1e59930a500c4
Naaashi_ <iccang43214@gmail.com>
2025-12-01 00:29:06 +0800

    Initial commit

---
+++ src/main/java/me/nashi/nashCommand/NashUtils.java
@@ -0,0 +1,17 @@
+package me.nashi.nashCommand;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class NashUtils extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
